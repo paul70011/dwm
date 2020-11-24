860eae99d35b89b3bf3e31bb3b73e4981cdbad1b
Paul Steinebach <paul70011@live.de>
2020-11-24 14:16:02 +0100

    Added movestack patch

--- config.h
+++ config.h
@@ -67,6 +67,7 @@ static const char *volumemutecmd[] = {"amixer", "sset", "'Master'", "toggle", NU
 static const char *scrotcmd[]  = { "scrot", "-e", "'sxiv ~/$f '",  NULL };
 static const char *scrotfocusedcmd[]  = { "scrot", "--focused", "-e", "'mv ~/$f ~/Pictures'", NULL };
 
+#include "movestack.c"
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
@@ -86,6 +87,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_Down,   setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_Up,     setmfact,       {.f = +0.05} },
+	{ MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
