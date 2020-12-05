0a9567b87ff8b548b083514dc65db2341c4d0eb1
Paul Steinebach <paul70011@live.de>
2020-12-05 17:16:09 +0100

    Increased number of clients in master area to 3

--- config.h
+++ config.h
@@ -33,7 +33,7 @@ static const Rule rules[] = {
 
 /* layout(s) */
 static const float mfact     = 0.5; /* factor of master area size [0.05..0.95] */
-static const int nmaster     = 1;    /* number of clients in master area */
+static const int nmaster     = 3;   /* number of clients in master area */
 static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
