2ebc1fe9dfa5bb15c8082c386583a22813cd7dc9
Paul Steinebach <paul70011@live.de>
2021-01-14 03:38:28 +0100

    Changed colors to solarized dark theme

--- config.h
+++ config.h
@@ -8,15 +8,15 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=16" };
 static const char dmenufont[]       = "monospace:size=16";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_accent[]      = "#005577";
+static const char col_bg[]       	= "#002b36";
+static const char col_gray1[]       = "#586e75";
+static const char col_gray2[]       = "#657b83";
+static const char col_gray3[]       = "#839496";
+static const char col_accent[]      = "#268bd2";
 static const char *colors[][3]      = {
 	/*               fg         bg          border   */
-	[SchemeNorm] = { col_gray3, col_gray1,  col_gray2  },
-	[SchemeSel]  = { col_gray4, col_accent, col_accent },
+	[SchemeNorm] = { col_gray2, col_bg,  col_gray1  },
+	[SchemeSel]  = { col_gray3, col_accent, col_accent },
 };
 
 /* tagging */
@@ -58,7 +58,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_accent, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_bg, "-nf", col_gray2, "-sb", col_accent, "-sf", col_gray3, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *brightnessinccmd[] = {"xbacklight", "-inc", "5", NULL};
 static const char *brightnessdeccmd[] = {"xbacklight", "-dec", "5", NULL};
